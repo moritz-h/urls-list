ca71e0a2ffc3fefa6ce0eb871e68a852fe30acb6
Moritz H <mo@blog-srv.net>
2018-12-30 18:47:18 +0100

    use new clipboard api (>= Firefox 63)

--- background.js
+++ background.js
@@ -6,45 +6,11 @@ browser.contextMenus.create({
 browser.contextMenus.onClicked.addListener(function (info, tab) {
   if (info.menuItemId === "url-list-copy-urls") {
     browser.tabs.query({currentWindow: true}).then((tabs) => {
-      // Use active tab for copy to clipboard, so it is possible to open
-      // the context menu on inactive tabs. Save clicked tab as fallback.
-      let activeTabId = tab.id;
-      let urls = '';
-      for (let tab of tabs) {
-        urls += tab.url + "\r\n";
-        if (tab.active) {
-          activeTabId = tab.id;
-        }
-      }
+      let urls = tabs.map(tab => tab.url).join('\r\n');
 
-      // example source: https://github.com/mdn/webextensions-examples/tree/master/context-menu-copy-link-with-types
-
-      // The example will show how data can be copied, but since background
-      // pages cannot directly write to the clipboard, we will run a content
-      // script that copies the actual content.
-
-      // clipboard-helper.js defines function copyToClipboard.
-      const code = "copyToClipboard(" + JSON.stringify(urls) + ");";
-
-      browser.tabs.executeScript({
-        code: "typeof copyToClipboard === 'function';",
-      }).then(function (results) {
-        // The content script's last expression will be true if the function
-        // has been defined. If this is not the case, then we need to run
-        // clipboard-helper.js to define function copyToClipboard.
-        if (!results || results[0] !== true) {
-          return browser.tabs.executeScript(activeTabId, {
-            file: "clipboard-helper.js",
-          });
-        }
-      }).then(function () {
-        return browser.tabs.executeScript(activeTabId, {
-          code,
-        });
-      }).catch(function (error) {
-        // This could happen if the extension is not allowed to run code in
-        // the page, for example if the tab is a privileged page.
-        console.error("Failed to copy text: " + error);
+      navigator.clipboard.writeText(urls).then(function() {
+        // success
+      }, function() {
         notifyError();
       });
     });
@@ -56,6 +22,6 @@ function notifyError() {
     "type": "basic",
     "iconUrl": browser.extension.getURL("icons/error.svg"),
     "title": "Error!",
-    "message": "Cannot write to clipboard on settings tab!"
+    "message": "Writing to clipboard is not possible!"
   });
 }
